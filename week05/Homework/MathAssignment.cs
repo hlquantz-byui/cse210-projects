900c24522bb7bfed4c8ba78d632bebabd8d70ebf
Hunter Quantz <qua24002@byui.edu>
2025-04-01 20:45:17 -0600

    Finished W05 Learning Activity: Inheritance

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,14 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection, _problems;
+    public MathAssignment(string name, string topic, string textbookSection, string problems) : base(name, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"{_textbookSection} {_problems}";
+    }
+}
